bde8131b060c15b0a1b63aab6f7e5869bf196e15
Tae Hyeon Jo <hillskuti@naver.com>
2022-09-04 15:25:43 +0900

    타입스크립트 기본 사용법

--- first-project/js-only.js
+++ first-project/js-only.js
@@ -1,11 +1,20 @@
-const button = document.querySelector("button");
-const input1 = document.getElementById("num1");
-const input2 = document.getElementById("num2");
+// const button = document.querySelector('button');
+// const input1 = document.getElementById('num1');
+// const input2 = document.getElementById('num2');
 
-function add(num1, num2) {
-  return num1 + num2;
-}
+// function add(num1, num2) {
+//   if (typeof num1 === 'number' && typeof num2 === 'number') {
+//     console.log('형변환 X');
+//     return num1 + num2;
+//   } else {
+//     // number로 형변환
+//     console.log('형변환');
+//     return +num1 + +num2;
+//   }
+// }
 
-button.addEventListener("click", function() {
-  console.log(add(input1.value, input2.value));
-});
+// button.addEventListener('click', function () {
+//   // it's important to know that in JavaScript, when you access the value of an input element.
+//   // it's always a string, always, no matter which type of input this is.
+//   console.log(add(input1.value, input2.value));
+// });
