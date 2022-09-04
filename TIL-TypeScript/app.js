938e8a23c27396f9b6a1950ed01fd3f7b4fbb321
Tae Hyeon Jo <hillskuti@naver.com>
2022-09-04 21:20:36 +0900

    Working with Numbers, Strings & Booleans

--- TIL-TypeScript/app.js
+++ TIL-TypeScript/app.js
@@ -1,7 +1,17 @@
-function add(n1, n2) {
-    return n1 + n2;
+function add(n1, n2, showResult, phrase) {
+    //   if (typeof n1 !== 'number' || typeof n2 !== 'number') {
+    //     throw new Error('Incorrect input!');
+    //   }
+    if (showResult) {
+        console.log(phrase + n1 + n2);
+    }
+    else {
+        return n1 + n2;
+    }
 }
-var number1 = 5;
+// 타입스크립트에서는 아래의 5와 5.0의 값이 동일하게 인식된다.
+var number1 = 5; // 5.0
 var number2 = 2.8;
-var result = add(number1, number2);
-console.log(result);
+var printResult = true;
+var resultPhrase = 'Result is:';
+add(number1, number2, printResult, resultPhrase, resultPhrase);
--- TIL-TypeScript/app.ts
+++ TIL-TypeScript/app.ts
@@ -1,9 +1,22 @@
-function add(n1: number, n2: number) {
-  return n1 + n2;
+function add(n1: number, n2: number, showResult: boolean, phrase: string) {
+  //   if (typeof n1 !== 'number' || typeof n2 !== 'number') {
+  //     throw new Error('Incorrect input!');
+  //   }
+
+  const result = n1 + n2;
+  if (showResult) {
+    // String과 number를 조합하였기 때문에 버그가 발생.
+    //  console.log(phrase + n1 + n2); // Result is : 52.8
+
+    console.log(phrase + result); // Result is : 7.8
+  } else {
+    return result;
+  }
 }
-
-const number1 = 5;
+// 타입스크립트에서는 아래의 5와 5.0의 값이 동일하게 인식된다.
+const number1 = 5; // 5.0
 const number2 = 2.8;
+const printResult = true;
+const resultPhrase = 'Result is: ';
 
-const result = add(number1, number2);
-console.log(result);
+add(number1, number2, printResult, resultPhrase, resultPhrase);
