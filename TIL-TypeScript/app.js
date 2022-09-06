22c635e0c0fbdac0e9d83b3eab6d9add26420052
Tae Hyeon Jo <hillskuti@naver.com>
2022-09-06 21:05:11 +0900

    Union Types

--- TIL-TypeScript/app.js
+++ TIL-TypeScript/app.js
@@ -1,47 +1,13 @@
-// // TypeScript doesn't support any type of property because we don't tell it anything about the object.
-// const person: {
-//   name: string;
-//   age: number;
-// } = {
-// const person: {
-//   name: string;
-//   age: number;
-//   hobbies: string[];
-//   role: [number, string]; // 튜플 타입
-// } = {
-//   name: 'Maximilian',
-//   age: 30,
-//   hobbies: ['Sports', 'Cooking'],
-//   role: [2, 'author'],
-// }
-// const ADMIN = 0;
-// const READ_ONLY = 1;
-// const AUTHOR = 2;
-var Role;
-(function (Role) {
-    Role[Role["ADMIN"] = 5] = "ADMIN";
-    Role[Role["READ_ONLY"] = 100] = "READ_ONLY";
-    Role[Role["AUTHOR"] = 200] = "AUTHOR";
-})(Role || (Role = {}));
-var person = {
-    name: 'Maximilian',
-    age: 30,
-    hobbies: ['Sports', 'Cooking'],
-    role: Role.ADMIN
-};
-// person.role.push('admin'); //? 튜플에서 push가 가능 (예외 기억)
-// person.role[1] = 10;
-// person.role = [0, 'ggg', 'aaa'];
-console.log(person.role);
-var favoriteActivities;
-// let favoriteActivities: any[];
-favoriteActivities = ['Sports'];
-console.log(person.name);
-for (var _i = 0, _a = person.hobbies; _i < _a.length; _i++) {
-    var hobby = _a[_i];
-    console.log(hobby.toUpperCase());
-    //   console.log(hobby.map()); // ERROR
+function combine(input1, input2) {
+    if (typeof input1 === 'number' && typeof input2 === 'number') {
+        var result = input1 + input2;
+    }
+    else {
+        result = input1.toString() + input2.toString();
+    }
+    return result;
 }
-if (person.role === Role.AUTHOR) {
-    console.log('is author');
-}
+var combinedAges = combine(30, 26);
+console.log(combinedAges);
+var combinedNames = combine('Max', 'Anna');
+console.log(combinedNames);
--- TIL-TypeScript/app.ts
+++ TIL-TypeScript/app.ts
@@ -1,54 +1,14 @@
-// // TypeScript doesn't support any type of property because we don't tell it anything about the object.
-// const person: {
-//   name: string;
-//   age: number;
-// } = {
-
-// const person: {
-//   name: string;
-//   age: number;
-//   hobbies: string[];
-//   role: [number, string]; // 튜플 타입
-// } = {
-//   name: 'Maximilian',
-//   age: 30,
-//   hobbies: ['Sports', 'Cooking'],
-//   role: [2, 'author'],
-// }
-
-// const ADMIN = 0;
-// const READ_ONLY = 1;
-// const AUTHOR = 2;
-
-enum Role {
-  ADMIN = 5,
-  READ_ONLY = 100,
-  AUTHOR = 200,
+function combine(input1: number | string, input2: number | string) {
+  if (typeof input1 === 'number' && typeof input2 === 'number') {
+    const result = input1 + input2;
+  } else {
+    result = input1.toString() + input2.toString();
+  }
+  return result;
 }
 
-const person = {
-  name: 'Maximilian',
-  age: 30,
-  hobbies: ['Sports', 'Cooking'],
-  role: Role.ADMIN,
-};
+const combinedAges = combine(30, 26);
+console.log(combinedAges);
 
-// person.role.push('admin'); //? 튜플에서 push가 가능 (예외 기억)
-// person.role[1] = 10;
-// person.role = [0, 'ggg', 'aaa'];
-console.log(person.role);
-
-let favoriteActivities: string[];
-// let favoriteActivities: any[];
-favoriteActivities = ['Sports'];
-
-console.log(person.name);
-
-for (const hobby of person.hobbies) {
-  console.log(hobby.toUpperCase());
-  //   console.log(hobby.map()); // ERROR
-}
-
-if (person.role === Role.AUTHOR) {
-  console.log('is author');
-}
+const combinedNames = combine('Max', 'Anna');
+console.log(combinedNames);
